611036c8e95c7123416ab21da0cef081419a03db
Samandar <laziz.go99@gmail.com>
2022-12-07 00:27:43 +0500

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,26 @@ import Card from "./components/Card";
 import Header from "./components/Header";
 
 const arr = [
-  { name: "Мужские Кроссовки Nike Blazer Mid Suede", price: 12999 },
-  { name: "Мужские Кроссовки Nike Air Max 270", price: 78892 },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: 1299,
+    imageUrl: "/img/sneakers/image1.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: 7892,
+    imageUrl: "/img/sneakers/image2.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 8482,
+    imageUrl: "/img/sneakers/image3.jpg",
+  },
+  {
+    name: "Мужские Кроссовки Jordan Air Jordan 11",
+    price: 11299,
+    imageUrl: "/img/sneakers/image4.jpg",
+  },
 ];
 
 function App() {
@@ -22,7 +40,9 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card arr={arr} />
+          {arr.map((obj,index) => (
+            <Card props={obj} key={index}/>
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ props }) => {
+  
+  
   return (
     <div className="card">
       <div className="favorite">
         <img src="/img/Group90.svg" alt="" />
       </div>
 
-      <img width={133} height={112} src="/img/sneakers/image1.jpg" alt="" />
-      <h5 className="marginbottom">Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="" />
+      <h5 className="marginbottom">{props.name}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Цена:</span>
-          <b>12 999 руб</b>
+          <b>{props.price} руб</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/img/pluses.svg" alt="" />
